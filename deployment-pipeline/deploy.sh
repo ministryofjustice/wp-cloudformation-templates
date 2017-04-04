fae2d510240500d57c3ff4d32792a4ea902d1135
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2017-04-04 16:38:37 +0100

    Suppress unwanted errors during stack creation.

--- deployment-pipeline/deploy.sh
+++ deployment-pipeline/deploy.sh
@@ -54,7 +54,7 @@ PARAMS="$PARAMS ParameterKey=ComposerUser,ParameterValue=$COMPOSER_USER"
 PARAMS="$PARAMS ParameterKey=ComposerPassword,ParameterValue=$COMPOSER_PASSWORD"
 PARAMS="$PARAMS ParameterKey=LambdaS3ObjectKey,ParameterValue=$LAMBDA_S3_OBJECT_KEY"
 
-aws cloudformation describe-stacks --stack-name "$STACK_NAME" > /dev/null
+aws cloudformation describe-stacks --stack-name "$STACK_NAME" > /dev/null 2>&1
 if [ $? -eq 0 ]
 then
 	echo "Updating CloudFormation stack"
